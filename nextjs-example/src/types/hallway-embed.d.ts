c2f576cdbddafebe2618c28b0a2861f59bfefe04
Matt Rossman <matt@joinhallway.com>
2025-06-06 10:00:41 -0400

    Remove dynamic type imports

--- nextjs-example/src/types/hallway-embed.d.ts
+++ nextjs-example/src/types/hallway-embed.d.ts
@@ -1,31 +1,18 @@
 import "react"
 
-// Import types dynamically so our declarations stay global https://stackoverflow.com/a/51114250
-/* eslint-disable @typescript-eslint/consistent-type-imports */
+import type {
+  HallwayEmbedBase,
+  HallwayEmbedBaseAttributes,
+  HallwayEmbedMinimized,
+  HallwayEmbedMinimizedAttributes,
+  HallwayEmbedExpanded,
+  HallwayEmbedExpandedAttributes,
+} from "./embed-loader";
 
 type CustomElementProps<
-	T extends HTMLElement,
-	A extends Record<string, unknown>,
+  T extends HTMLElement,
+  A extends Record<string, unknown>,
 > = React.DetailedHTMLProps<React.HTMLAttributes<T>, T> & A;
-
-// Constructors
-
-type HallwayEmbedMinimized =
-	import("./embed-loader").HallwayEmbedMinimized;
-type HallwayEmbedExpanded =
-	import("./embed-loader").HallwayEmbedExpanded;
-type HallwayEmbedBase =
-	import("./embed-loader").HallwayEmbedBase;
-
-// Attributes
-
-type HallwayEmbedMinimizedAttributes =
-	import("./embed-loader").HallwayEmbedMinimizedAttributes;
-type HallwayEmbedExpandedAttributes =
-	import("./embed-loader").HallwayEmbedExpandedAttributes;
-type HallwayEmbedBaseAttributes =
-	import("./embed-loader").HallwayEmbedBaseAttributes;
-
 
 declare module "react" {
   declare namespace JSX {
